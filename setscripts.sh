24f6f77402997d69bba81fb0d29387c8e3342748
Luke Smith <lukesmudgesmith@gmail.com>
2017-04-18 14:34:29 +0100

    Added in script to automatically generate release notes between the 2 last versions

--- setscripts.sh
+++ setscripts.sh
@@ -5,4 +5,6 @@ rm /usr/bin/devenv
 cp devenv.sh /usr/bin/devenv
 rm /usr/bin/updatepc
 cp updatepc.sh /usr/bin/updatepc
+rm /usr/bin/gen-git-release
+cp gen-git-release.sh /usr/bin/gen-git-release
 echo 'Scripts copied'
