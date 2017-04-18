5139f5270fc41767ea4ec4fc75054c22078b2b0b
Luke Smith <lukesmudgesmith@gmail.com>
2017-04-18 14:41:18 +0100

    updated git changelog generator name

--- setscripts.sh
+++ setscripts.sh
@@ -5,6 +5,6 @@ rm /usr/bin/devenv
 cp devenv.sh /usr/bin/devenv
 rm /usr/bin/updatepc
 cp updatepc.sh /usr/bin/updatepc
-rm /usr/bin/gen-git-release
-cp gen-git-release.sh /usr/bin/gen-git-release
+rm /usr/bin/gen-git-release-notes
+cp gen-git-release-notes.sh /usr/bin/gen-git-release-notes
 echo 'Scripts copied'
