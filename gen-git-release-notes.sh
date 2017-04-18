745b85f90783a7da5adcb68133e4b8c628b2f79e
Luke Smith <lukesmudgesmith@gmail.com>
2017-04-18 15:08:01 +0100

    Changed method of printing previous and current git versions

--- gen-git-release-notes.sh
+++ gen-git-release-notes.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 echo '=== Generating git release notes...'
-PreviousAndCurrentGitTag=`git log --tags --no-walk --pretty="format:%d" | sed 2q | sed 's/[()]//g' | sed s/,[^,]*$// | sed  's ......  '`
-PreviousGitTag=`echo $PreviousAndCurrentGitTag | cut -f 2 -d ' '`
-CurrentGitTag=`echo $PreviousAndCurrentGitTag | cut -f 1 -d ' '`
+PreviousAndCurrentGitTag=`git tag | sort -V | tail -2`
+PreviousGitTag=`echo $PreviousAndCurrentGitTag | cut -f 1 -d ' '`
+CurrentGitTag=`echo $PreviousAndCurrentGitTag | cut -f 2 -d ' '`
 
-git release-notes ${PreviousGitTag}..${CurrentGitTag} markdown > changelog-${PreviousGitTag}..${CurrentGitTag}.md
+git-release-notes ${PreviousGitTag}..${CurrentGitTag} markdown > changelog-${PreviousGitTag}..${CurrentGitTag}.md
 echo "=== Release notes generated -> changelog-${PreviousGitTag}..${CurrentGitTag}.md"
